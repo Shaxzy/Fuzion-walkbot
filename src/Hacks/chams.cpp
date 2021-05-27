96f164fe2f8607a641670fbc54994a4afa6c922b
Jane Doe <henning.brods@gmail.com>
2021-05-27 17:50:42 +0200

    Add files via upload

---
+++ src/Hacks/chams.cpp
@@ -0,0 +1,186 @@
+#include "chams.h"
+
+#include "../Utils/xorstring.h"
+#include "../Utils/entity.h"
+#include "../settings.h"
+#include "../interfaces.h"
+
+IMaterial* materialChams;
+IMaterial* materialChamsIgnorez;
+IMaterial* materialChamsFlat;
+IMaterial* materialChamsFlatIgnorez;
+IMaterial* materialChamsArms;
+IMaterial* materialChamsWeapons;
+
+typedef void (*DrawModelExecuteFn) (void*, void*, void*, const ModelRenderInfo_t&, matrix3x4_t*);
+
+static void DrawPlayer(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t* pCustomBoneToWorld)
+{
+	if (!Settings::ESP::Chams::enabled)
+		return;
+
+	C_BasePlayer* localplayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
+	if (!localplayer)
+		return;
+
+	C_BasePlayer* entity = (C_BasePlayer*) entityList->GetClientEntity(pInfo.entity_index);
+	if (!entity
+		|| entity->GetDormant()
+		|| !entity->GetAlive())
+		return;
+
+	if (entity == localplayer && !Settings::ESP::Filters::localplayer)
+		return;
+
+	if (!Entity::IsTeamMate(entity, localplayer) && !Settings::ESP::Filters::enemies)
+		return;
+
+	if (entity != localplayer && Entity::IsTeamMate(entity,localplayer) && !Settings::ESP::Filters::allies)
+		return;
+
+	IMaterial* visible_material = nullptr;
+	IMaterial* hidden_material = nullptr;
+
+	switch (Settings::ESP::Chams::type)
+	{
+		case ChamsType::CHAMS:
+		case ChamsType::CHAMS_XQZ:
+			visible_material = materialChams;
+			hidden_material = materialChamsIgnorez;
+			break;
+		case ChamsType::CHAMS_FLAT:
+		case ChamsType::CHAMS_FLAT_XQZ:
+			visible_material = materialChamsFlat;
+			hidden_material = materialChamsFlatIgnorez;
+			break;
+	}
+	
+	ImColor visColor;
+	ImColor color;
+	
+	if (entity == localplayer)
+	{
+		visColor = Settings::ESP::Chams::localplayerColor.Color(entity);
+		color = visColor;
+		
+		color.Value.x *= 0.45f; // does same thing as color *= 0.45
+		color.Value.y *= 0.45f;
+		color.Value.z *= 0.45f;
+	}
+	else if (Entity::IsTeamMate(entity, localplayer))
+	{
+		visColor = Settings::ESP::Chams::allyVisibleColor.Color(entity);
+		color = Settings::ESP::Chams::allyColor.Color(entity);
+	}
+	else if (!Entity::IsTeamMate(entity, localplayer))
+	{
+		visColor = Settings::ESP::Chams::enemyVisibleColor.Color(entity);
+		color = Settings::ESP::Chams::enemyColor.Color(entity);
+	}
+	else
+	{
+		return;
+	}
+	
+	visible_material->ColorModulate(visColor);
+	hidden_material->ColorModulate(color);
+	
+	if (entity->GetImmune()) {
+		visible_material->AlphaModulate(visColor.Value.w / 2);
+		hidden_material->AlphaModulate(color.Value.w / 2);
+	} else {
+		visible_material->AlphaModulate(visColor.Value.w);
+		hidden_material->AlphaModulate(color.Value.w);
+	}
+
+	if (!Settings::ESP::Filters::legit && (Settings::ESP::Chams::type == ChamsType::CHAMS_XQZ || Settings::ESP::Chams::type == ChamsType::CHAMS_FLAT_XQZ))
+	{
+		modelRender->ForcedMaterialOverride(hidden_material);
+		modelRenderVMT->GetOriginalMethod<DrawModelExecuteFn>(21)(thisptr, context, state, pInfo, pCustomBoneToWorld);
+	}
+
+	modelRender->ForcedMaterialOverride(visible_material);
+	// No need to call DME again, it already gets called in DrawModelExecute.cpp
+}
+
+static void DrawWeapon(const ModelRenderInfo_t& pInfo)
+{
+	if (!Settings::ESP::Chams::Weapon::enabled)
+		return;
+
+    	//turn weapon chams off while scoped so you can actually see with COD guns while scoped -Crazily.
+    	C_BasePlayer *localPlayer = (C_BasePlayer *) entityList->GetClientEntity(engine->GetLocalPlayer());
+    	if (localPlayer->IsScoped())
+		return;
+
+	std::string modelName = modelInfo->GetModelName(pInfo.pModel);
+	IMaterial* mat = materialChamsWeapons;
+
+	if (!Settings::ESP::Chams::Weapon::enabled)
+		mat = material->FindMaterial(modelName.c_str(), TEXTURE_GROUP_MODEL);
+
+	mat->ColorModulate(Settings::ESP::Chams::Weapon::color.Color());
+	mat->AlphaModulate(Settings::ESP::Chams::Weapon::color.Color().Value.w);
+
+	mat->SetMaterialVarFlag(MATERIAL_VAR_WIREFRAME, Settings::ESP::Chams::Weapon::type == WeaponType::WIREFRAME);
+	mat->SetMaterialVarFlag(MATERIAL_VAR_NO_DRAW, Settings::ESP::Chams::Weapon::type == WeaponType::NONE);
+	modelRender->ForcedMaterialOverride(mat);
+}
+
+static void DrawArms(const ModelRenderInfo_t& pInfo)
+{
+	if (!Settings::ESP::Chams::Arms::enabled)
+		return;
+
+	std::string modelName = modelInfo->GetModelName(pInfo.pModel);
+	IMaterial* mat = materialChamsArms;
+
+	if (!Settings::ESP::Chams::Arms::enabled)
+		mat = material->FindMaterial(modelName.c_str(), TEXTURE_GROUP_MODEL);
+
+	mat->ColorModulate(Settings::ESP::Chams::Arms::color.Color());
+	mat->AlphaModulate(Settings::ESP::Chams::Arms::color.Color().Value.w);
+
+	mat->SetMaterialVarFlag(MATERIAL_VAR_WIREFRAME, Settings::ESP::Chams::Arms::type == ArmsType::WIREFRAME);
+	mat->SetMaterialVarFlag(MATERIAL_VAR_NO_DRAW, Settings::ESP::Chams::Arms::type == ArmsType::NONE);
+	modelRender->ForcedMaterialOverride(mat);
+}
+
+void Chams::DrawModelExecute(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t* pCustomBoneToWorld)
+{
+	if (!engine->IsInGame())
+		return;
+
+	if (!Settings::ESP::enabled)
+		return;
+
+	if (!pInfo.pModel)
+		return;
+
+	static bool materialsCreated = false;
+	if (!materialsCreated)
+	{
+		materialChams = Util::CreateMaterial(XORSTR("VertexLitGeneric"), XORSTR("VGUI/white_additive"), false, true, true, true, true);
+		materialChamsIgnorez = Util::CreateMaterial(XORSTR("VertexLitGeneric"), XORSTR("VGUI/white_additive"), true, true, true, true, true);
+		materialChamsFlat = Util::CreateMaterial(XORSTR("UnlitGeneric"), XORSTR("VGUI/white_additive"), false, true, true, true, true);
+		materialChamsFlatIgnorez = Util::CreateMaterial(XORSTR("UnlitGeneric"), XORSTR("VGUI/white_additive"), true, true, true, true, true);
+		materialChamsArms = Util::CreateMaterial(XORSTR("VertexLitGeneric"), XORSTR("VGUI/white_additive"), false, true, true, true, true);
+		materialChamsWeapons = Util::CreateMaterial(XORSTR("VertexLitGeneric"), XORSTR("VGUI/white_additive"), false, true, true, true, true);
+		materialsCreated = true;
+	}
+
+	const char *modelName = modelInfo->GetModelName(pInfo.pModel);
+
+	if (strstr(modelName, XORSTR("models/player")))
+	{
+		DrawPlayer(thisptr, context, state, pInfo, pCustomBoneToWorld);
+	}
+	else if (strstr(modelName, XORSTR("arms")))
+	{
+		DrawArms(pInfo);
+	}
+	else if (strstr(modelName, XORSTR("weapon")))
+	{
+		DrawWeapon(pInfo);
+	}
+}
